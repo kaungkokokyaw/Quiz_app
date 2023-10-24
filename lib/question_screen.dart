460e93385232f54d8eb8a75cc67ff1c542bbf9a5
kaungkokokyaw <kaungkokokyaw20@gmail.com>
2023-10-24 11:31:53 +0630

    initial commit

---
+++ lib/question_screen.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:udemy_quiz_app/answer_button.dart';
+import 'package:udemy_quiz_app/data/question.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class QuestionScreen extends StatefulWidget {
+  const QuestionScreen({
+    super.key,
+    required this.onSelectedAnswer,
+  });
+
+  final void Function(String answer) onSelectedAnswer;
+
+  @override
+  State<QuestionScreen> createState() => _QuestionScreenState();
+}
+
+class _QuestionScreenState extends State<QuestionScreen> {
+  var currentQuestionIndex = 0;
+
+  void answerQuestion(String selectedAnswer) {
+    setState(() {
+      widget.onSelectedAnswer(selectedAnswer);
+      // currentQuestionIndex = currentQuestionIndex +1;
+      // currentQuestionIndex +=1;
+      currentQuestionIndex++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final currentQuestion = questions[currentQuestionIndex];
+
+    return SizedBox(
+      width: double.infinity,
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: GoogleFonts.lato(
+                color: Colors.white,
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            ...currentQuestion.getShuffledAnswer().map((answer) {
+              return AnswerButton(
+                answerText: answer,
+                OnTap: () {
+                  answerQuestion(answer);
+                },
+              );
+            }),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/quiz.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:udemy_quiz_app/data/question.dart';
+import 'package:udemy_quiz_app/question_screen.dart';
+import 'package:udemy_quiz_app/start_screen.dart';
+import 'package:udemy_quiz_app/result_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() => _QuizState();
+}
+
+class _QuizState extends State<Quiz> {
+  List<String> selectedAnswer = [];
+  var activeScreen = 'start-screen';
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = 'questions-screen';
+    });
+  }
+
+  void userChooseAnswer(String answer) {
+    selectedAnswer.add(answer);
+
+    if (selectedAnswer.length == questions.length) {
+      setState(() {
+        selectedAnswer = [];
+        activeScreen = 'result-screen';
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget screenWidget = StartScreen(switchScreen);
+
+    if (activeScreen == 'questions-screen') {
+      screenWidget = QuestionScreen(
+        onSelectedAnswer: userChooseAnswer,
+      );
+    }
+
+    if (activeScreen == 'result-screen') {
+      screenWidget = ResultScreen(
+        chosenAnswer: selectedAnswer,
+      );
+    }
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 113, 177, 230),
+                Color.fromARGB(255, 24, 69, 107)
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: screenWidget,
+        ),
+      ),
+    );
+  }
+}
